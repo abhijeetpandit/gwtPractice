c88d36ac23eaf01882111eb1a703ad23b6e06f4b
apandit <apandit@CAPMPT2040036.CAPMPTDC.COM>
2016-01-28 17:28:54 +0530

    First gwt project

---
+++ Hello GWT/src/org/abhi/hello/client/Hello_GWT.java
@@ -0,0 +1,33 @@
+package org.abhi.hello.client;
+
+import org.abhi.hello.shared.FieldVerifier;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Hello_GWT implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Window.alert("Hello THIRD!! GWT user!!!!");
+		
+	}
+	
+}
